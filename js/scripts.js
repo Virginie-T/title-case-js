f39bd824b78dd246ec9e088779da32c3a3894a3b
Bryan Borgeson & Virginie Trubiano <student@epicodus.com>
2015-04-07 13:51:52 -0700

    7 complet tests

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,28 @@ function titleCase(word) {
 
     var lowerWord = word.toLowerCase();
 
+    var noCap = ["a", "the"];
+
     var list_of_words = lowerWord.split(" ");
 
     var wordArray = [];
 
-    // var Upper1 = list_of_words[0].charAt(0).toUpperCase();
-    // var Upper2 = list_of_words[1].charAt(0).toUpperCase();
-
-    // wordArray.push(Upper1, Upper2);
-
     for (var wordz = 0; wordz < list_of_words.length; wordz++) {
-        var UpperCase = list_of_words[wordz].charAt(0).toUpperCase();
-        var temp = list_of_words[wordz].substring(1);
-        wordArray.push(UpperCase + temp);
+
+        if (list_of_words[wordz] === noCap[0] || list_of_words[wordz] === noCap[1]) {
+            wordArray.push(list_of_words[wordz]);
+        }
+
+        else {
+            var UpperCase = list_of_words[wordz].charAt(0).toUpperCase();
+            var temp = list_of_words[wordz].substring(1);
+            wordArray.push(UpperCase + temp);
+        }
     }
 
-    return wordArray.join(" ");
+    var stringArray = wordArray.join(" ");
+
+    var final = stringArray[0].toUpperCase() + stringArray.substring(1);
+
+    return final;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,20 @@ describe('titleCase', function() {
     it("capitalizes the first letter of two different words", function() {
         expect(titleCase("deaD CAT")).to.equal("Dead Cat");
     });
+
+    it("capitalizes the first letter of three different words", function() {
+        expect(titleCase("sUpEr deaD CAT")).to.equal("Super Dead Cat");
+    });
+
+    it("does not capitalize the word 'a' in the middle of a sentence", function() {
+        expect(titleCase("you are a neat person")).to.equal("You Are a Neat Person");
+    });
+
+    it("capitalizes 'a' at the first position in a sentence", function() {
+        expect(titleCase("a CAT")).to.equal("A Cat");
+    });
+
+    it("capitalizes 'the' at the first position in a sentence", function() {
+        expect(titleCase("thE CAT")).to.equal("The Cat");
+    });
 });
